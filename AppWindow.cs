43a0dea8e76b88c30d705279d13c9b5bdb0c1fc2
Friedchicken <paololego20+github@gmail.com>
2022-05-26 12:57:32 +0200

    moved config handling to Config.cs, automatically reload the window when config.ini is edited

--- AppWindow.cs
+++ AppWindow.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using SFML.Graphics;
 using SFML.System;
 using SFML.Window;
@@ -11,121 +12,94 @@ namespace KeyOverlay
 {
     public class AppWindow
     {
-        private readonly RenderWindow _window;
-        private readonly List<Key> _keyList = new();
-        private readonly List<RectangleShape> _squareList = new();
-        private readonly float _barSpeed;
-        private readonly float _ratioX;
-        private readonly float _ratioY;
-        private readonly int _outlineThickness;
-        private readonly Color _backgroundColor;
-        private readonly Color _fontColor;
-        private readonly bool _fading;
-        private readonly bool _counter;
-        private readonly List<Drawable> _staticDrawables = new();
-        private readonly uint _maxFPS;
+        private RenderWindow _window;
+        private Vector2u _size;
+        private List<Key> _keyList;
+        private List<RectangleShape> _squareList;
+        private float _barSpeed;
+        private float _ratioX;
+        private float _ratioY;
+        private int _outlineThickness;
+        private Color _backgroundColor;
+        private Color _fontColor;
+        private bool _fading;
+        private bool _counter;
+        private List<Drawable> _staticDrawables;
+        private uint _maxFPS;
         private Clock _clock = new();
+        private Config _config;
+        private object _lock = new object();
 
 
         public AppWindow()
         {
-            var config = ReadConfig();
-            CheckConfig(config);
-            var general = config["General"];
-
-            _barSpeed = float.Parse(general["barSpeed"], CultureInfo.InvariantCulture);
-            _backgroundColor = CreateItems.CreateColor(general["backgroundColor"]);
-            _maxFPS = uint.Parse(general["fps"]);
-
-            //create keys which will be used to create the squares and text
-            foreach (var item in config["Keys"])
-            {
-                var key = new Key(item.Value);
-
-                if (config["Display"].ContainsKey(item.Key))
-                    key.setKeyLetter(config["Display"][item.Key]);
-
-                if (config["Colors"].ContainsKey(item.Key))
-                    key.setColor(CreateItems.CreateColor(config["Colors"][item.Key]));
-
-                if (config["Size"].ContainsKey(item.Key))
-                    key.setSize(uint.Parse(config["Size"][item.Key]));
-
-                _keyList.Add(key);
-            }
-
-            //create squares and add them to _staticDrawables list
-            _outlineThickness = int.Parse(general["outlineThickness"]);
-            var keySize = int.Parse(general["keySize"]);
-            var margin = int.Parse(general["margin"]);
-
-            var windowWidth = margin;
-            foreach (var key in _keyList)
-            {
-                windowWidth += keySize * (int)key._size + _outlineThickness * 2 + margin;
-            }
-            var windowHeight = general["height"];
-            _window = new RenderWindow(new VideoMode((uint)windowWidth, uint.Parse(windowHeight!)),
-                "KeyOverlay", Styles.Default);
-
-            //calculate screen ratio relative to original program size for easy resizing
-            _ratioX = windowWidth / 480f;
-            _ratioY = float.Parse(windowHeight) / 960f;
-
-            _squareList = CreateItems.CreateKeys(_keyList, keySize, _ratioX, _ratioY, margin, _outlineThickness);
-            foreach (var square in _squareList) _staticDrawables.Add(square);
-
-            // //create text and add it to _staticDrawables list
-            _fontColor = new Color(255, 255, 255, 255);
-            for (var i = 0; i < _keyList.Count; i++)
-            {
-                var text = CreateItems.CreateText(_keyList[i].KeyLetter, _squareList[i], _fontColor, false);
-                _staticDrawables.Add(text);
-            }
-
-            if (general["fading"] == "yes")
-                _fading = true;
-            if (general["counter"] == "yes")
-                _counter = true;
+            _window = new RenderWindow(new VideoMode(600, 800), "Key Overlay");
+            _config = new Config("config.ini", Initialize);
+            Initialize();
         }
 
-        private Dictionary<string, Dictionary<string, string>> ReadConfig()
+        public void Initialize()
         {
-            var objectDict = new Dictionary<string, Dictionary<string, string>>();
-            var file = File.ReadLines("config.ini").ToArray();
-            var current = "";
-            foreach (var line in file)
+            lock (_lock)
             {
-                Console.WriteLine(line);
-                if (line == "") continue;
-                if (line.StartsWith("["))
+                var general = _config["General"];
+
+                _barSpeed = float.Parse(general["barSpeed"], CultureInfo.InvariantCulture);
+                _backgroundColor = CreateItems.CreateColor(general["backgroundColor"]);
+                _maxFPS = uint.Parse(general["fps"]);
+
+                //create keys which will be used to create the squares and text
+                _keyList = new List<Key>();
+                foreach (var item in _config["Keys"])
                 {
-                    current = line.Substring(1, line.Length - 2);
-                    objectDict.Add(current, new());
+                    var key = new Key(item.Value);
+
+                    if (_config["Display"].ContainsKey(item.Key))
+                        key.setKeyLetter(_config["Display"][item.Key]);
+
+                    if (_config["Colors"].ContainsKey(item.Key))
+                        key.setColor(CreateItems.CreateColor(_config["Colors"][item.Key]));
+
+                    if (_config["Size"].ContainsKey(item.Key))
+                        key.setSize(uint.Parse(_config["Size"][item.Key]));
+
+                    _keyList.Add(key);
                 }
-                else
+
+                //create squares and add them to _staticDrawables list
+                _outlineThickness = int.Parse(general["outlineThickness"]);
+                var keySize = int.Parse(general["keySize"]);
+                var margin = int.Parse(general["margin"]);
+
+                var windowWidth = margin;
+                foreach (var key in _keyList)
                 {
-                    var key = line.Split('=')[0];
-                    var value = line.Split('=')[1];
-                    objectDict[current].Add(key, value);
+                    windowWidth += keySize * (int) key._size + _outlineThickness * 2 + margin;
                 }
 
-            }
-            return objectDict;
-        }
+                var windowHeight = int.Parse(general["height"]);
+                _size = new Vector2u((uint) windowWidth, (uint) windowHeight);
 
-        private void CheckConfig(Dictionary<string, Dictionary<string, string>> config)
-        {
+                //calculate screen ratio relative to original program size for easy resizing
+                _ratioX = windowWidth / 480f;
+                _ratioY = windowHeight / 960f;
 
-            string[] required = { "General", "Keys" };
-            string[] optional = { "Display", "Size", "Color" };
-            foreach (var name in required)
-            {
-                if (!config.ContainsKey(name)) throw new InvalidDataException("Missing required data from config file");
-            }
-            foreach (var name in optional)
-            {
-                if (!config.ContainsKey(name)) config.Add(name, new());
+                _staticDrawables = new List<Drawable>();
+                _squareList = CreateItems.CreateKeys(_keyList, keySize, _ratioX, _ratioY, margin, _outlineThickness);
+                foreach (var square in _squareList) _staticDrawables.Add(square);
+
+                // //create text and add it to _staticDrawables list
+                _fontColor = new Color(255, 255, 255, 255);
+                for (var i = 0; i < _keyList.Count; i++)
+                {
+                    var text = CreateItems.CreateText(_keyList[i].KeyLetter, _squareList[i], _fontColor, false);
+                    _staticDrawables.Add(text);
+                }
+
+                if (general["fading"] == "yes")
+                    _fading = true;
+                if (general["counter"] == "yes")
+                    _counter = true;
             }
         }
 
@@ -152,48 +126,54 @@ namespace KeyOverlay
 
             while (_window.IsOpen)
             {
-                _window.Clear(_backgroundColor);
                 _window.DispatchEvents();
-                // //if no keys are being held fill the square with bg color
-                for (var i = 0; i < _keyList.Count; i++)
-                {
-                    var key = _keyList[i];
 
-                    if (key.isKey && Keyboard.IsKeyPressed(key.KeyboardKey) ||
-                        !key.isKey && Mouse.IsButtonPressed(key.MouseButton))
+                _window.Size = _size;
+                _window.SetView(new View(new FloatRect(0, 0, _size.X, _size.Y)));
+                
+                _window.Clear(_backgroundColor);
+                
+                lock (_lock) {
+                    // //if no keys are being held fill the square with bg color
+                    for (var i = 0; i < _keyList.Count; i++)
                     {
-                        key.Hold++;
-                        _squareList[i].FillColor = key._colorPressed;
+                        var key = _keyList[i];
+
+                        if (key.isKey && Keyboard.IsKeyPressed(key.KeyboardKey) ||
+                                !key.isKey && Mouse.IsButtonPressed(key.MouseButton))
+                        {
+                            key.Hold++;
+                            _squareList[i].FillColor = key._colorPressed;
+                        }
+                        else
+                        {
+                            key.Hold = 0;
+                            _squareList[i].FillColor = _backgroundColor;
+                        }
                     }
-                    else
+
+                    MoveBars(_keyList, _squareList);
+
+                    foreach (var staticDrawable in _staticDrawables) _window.Draw(staticDrawable);
+
+                    for (var i = 0; i < _keyList.Count; i++)
                     {
-                        key.Hold = 0;
-                        _squareList[i].FillColor = _backgroundColor;
+                        var key = _keyList[i];
+
+                        if (_counter)
+                        {
+                            var text = CreateItems.CreateText(
+                                Convert.ToString(key.Counter),
+                                    _squareList[i],
+                                    Color.White,
+                                    true
+                                );
+                            _window.Draw(text);
+                        }
+                        foreach (var bar in key.BarList) _window.Draw(bar);
                     }
+                    _window.Draw(fadingSprite);
                 }
-
-                MoveBars(_keyList, _squareList);
-
-                foreach (var staticDrawable in _staticDrawables) _window.Draw(staticDrawable);
-
-                for (var i = 0; i < _keyList.Count; i++)
-                {
-                    var key = _keyList[i];
-
-                    if (_counter)
-                    {
-                        var text = CreateItems.CreateText(
-                            Convert.ToString(key.Counter),
-                            _squareList[i],
-                            Color.White,
-                            true
-                        );
-                        _window.Draw(text);
-                    }
-                    foreach (var bar in key.BarList) _window.Draw(bar);
-                }
-                _window.Draw(fadingSprite);
-
                 _window.Display();
             }
         }
---
+++ Config.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace KeyOverlay
+{
+    public class Config
+    {
+        private string name;
+        private Dictionary<string, Dictionary<string, string>> config;
+        private FileSystemWatcher watcher;
+        private Action callback;
+
+        public Config(string name, Action callback)
+        {
+            this.name = name;
+            this.callback = callback;
+            Load();
+
+            watcher = new FileSystemWatcher();
+            watcher.Path = ".";
+            watcher.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.LastAccess;
+            watcher.Filter = name;
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.EnableRaisingEvents = true;
+        }
+
+        public Dictionary<string, string> this[string key] => config[key];
+
+        private void Read()
+        {
+            var objectDict = new Dictionary<string, Dictionary<string, string>>();
+            var lines = File.ReadAllLines(name);
+            var current = "";
+            foreach (var line in lines)
+            {
+                if (line == "") continue;
+                if (line.StartsWith("["))
+                {
+                    current = line.Substring(1, line.Length - 2);
+                    objectDict.Add(current, new());
+                }
+                else
+                {
+                    var key = line.Split('=')[0];
+                    var value = line.Split('=')[1];
+                    objectDict[current].Add(key, value);
+                }
+
+            }
+            this.config = objectDict;
+        }
+
+        private void Check()
+        {
+            string[] required = { "General", "Keys" };
+            string[] optional = { "Display", "Size", "Color" };
+            foreach (var name in required)
+            {
+                if (!config.ContainsKey(name)) throw new InvalidDataException("Missing required data from config file");
+            }
+            foreach (var name in optional)
+            {
+                if (!config.ContainsKey(name)) config.Add(name, new());
+            }
+        }
+        public void Load()
+        {
+            Read();
+            Check();
+        }
+
+        private void OnChanged(object source, FileSystemEventArgs e)
+        {
+            // funky event *before* the file is actually saved
+            System.Threading.Thread.Sleep(100);
+            Load();
+            callback();
+        }
+    }
+}
